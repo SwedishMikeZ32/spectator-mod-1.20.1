4e55aeb43d3579868bc8f6d1463d2b501a43af01
GaminMike <Mikaeltylerrife@gmail.com>
2024-10-04 23:03:47 -0400

    Initial commit

---
+++ src/main/java/net/rife/spectator/mod/SpectatorModClient.java
@@ -0,0 +1,10 @@
+package net.rife.spectator.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpectatorModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
